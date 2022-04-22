69695ff487f393b13c1fa8e7e5d2b6f0d5acb353
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-04-22 11:33:14 +0900

    Initial Commit

---
+++ Shared/BlogrLandingPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlogrLandingPageApp.swift
+//  Shared
+//
+//  Created by Jinwook Kim on 2022/04/22.
+//
+
+import SwiftUI
+
+@main
+struct BlogrLandingPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
