e10e52c8986be71f4d291d4acb9394231c933510
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-04-23 07:08:55 +0900

    Add the footer item view.

--- Shared/Views/FooterGroupView.swift
+++ Shared/Views/FooterGroupView.swift
@@ -9,14 +9,35 @@ import SwiftUI
 
 struct FooterGroupView: View {
     let title: String
+    let items: [FooterItem]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 16) {
+            Text(title)
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+            VStack(spacing: 8) {
+                ForEach(items, id: \.self) { item in
+                    Button {
+                        // TODO: LINK TO item.link.
+                    } label: {
+                        Text(item.text)
+                            .font(.title2)
+                            .fontWeight(.regular)
+                            .foregroundColor(.white.opacity(0.5))
+                    }
+                }
+            }
+        }
     }
 }
 
 struct FooterGroupView_Previews: PreviewProvider {
+    static let previewItems = [FooterItem(text: "Overview", link: "#"), FooterItem(text: "Pricing", link: "#"), FooterItem(text: "Marketplace", link: "#")]
     static var previews: some View {
-        FooterGroupView(title: "Product")
+        FooterGroupView(title: "Product", items: previewItems)
+            .padding()
+            .background(Color("FooterBackgroundColor"))
             .previewLayout(.sizeThatFits)
     }
 }
