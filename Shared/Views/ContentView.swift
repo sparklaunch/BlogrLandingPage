1866b9c0a37db231da6319586bf3b7430fc9f00c
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-04-22 12:54:22 +0900

    Add the free view.

--- Shared/Views/ContentView.swift
+++ Shared/Views/ContentView.swift
@@ -17,6 +17,7 @@ struct ContentView: View {
                 RobustView()
                 InfrastructureView()
                 LaptopView()
+                FreeView()
             }
         }
         .edgesIgnoringSafeArea(.all)
---
+++ Shared/Views/FreeView.swift
@@ -0,0 +1,34 @@
+//
+//  FreeView.swift
+//  Blogr Landing Page (iOS)
+//
+//  Created by Jinwook Kim on 2022/04/22.
+//
+
+import SwiftUI
+
+struct FreeView: View {
+    var body: some View {
+        VStack(spacing: 24) {
+            Text("Free, open, simple")
+                .font(.title)
+                .fontWeight(.semibold)
+                .foregroundColor(Color("TitleColor"))
+            Text("Blogr is a free and open-source application backed by a large community of helpful developers. It supports features such as code syntax highlighting, RSS feeds, social media integration, third-party commenting tools, and works seamlessly with Google Analytics. The architecture is clean and is relatively easy to learn.")
+                .font(.body)
+                .fontWeight(.regular)
+                .foregroundColor(Color("BodyColor"))
+                .padding(.horizontal)
+                .lineSpacing(5)
+        }
+        .multilineTextAlignment(.center)
+        .padding(24)
+    }
+}
+
+struct FreeView_Previews: PreviewProvider {
+    static var previews: some View {
+        FreeView()
+            .previewLayout(.sizeThatFits)
+    }
+}
