c0afe7b46a1a237308125e93da3371b48dac9909
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-04-23 07:12:15 +0900

    Add the footer content view.

--- Shared/Views/FooterContentView.swift
+++ Shared/Views/FooterContentView.swift
@@ -9,18 +9,14 @@ import SwiftUI
 
 struct FooterContentView: View {
     var body: some View {
-        VStack(spacing: 24) {
+        VStack(spacing: 64) {
             Image("Logo")
                 .resizable()
                 .frame(width: 101, height: 40)
-            VStack {
-
-            }
-            VStack {
-
-            }
-            VStack {
-
+            VStack(spacing: 36) {
+                FooterGroupView(title: "Product", items: [FooterItem(text: "Overview", link: "#"), FooterItem(text: "Pricing", link: "#"), FooterItem(text: "Marketplace", link: "#"), FooterItem(text: "Features", link: "#"), FooterItem(text: "Integrations", link: "#")])
+                FooterGroupView(title: "Company", items: [FooterItem(text: "About", link: "#"), FooterItem(text: "Team", link: "#"), FooterItem(text: "Blog", link: "#"), FooterItem(text: "Careers", link: "#")])
+                FooterGroupView(title: "Connect", items: [FooterItem(text: "Contact", link: "#"), FooterItem(text: "Newsletter", link: "#"), FooterItem(text: "LinkedIn", link: "#")])
             }
         }
     }
@@ -29,6 +25,7 @@ struct FooterContentView: View {
 struct FooterContentView_Previews: PreviewProvider {
     static var previews: some View {
         FooterContentView()
+            .padding()
             .background(Color("FooterBackgroundColor"))
             .previewLayout(.sizeThatFits)
     }
